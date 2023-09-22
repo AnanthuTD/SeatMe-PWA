79b2481782f233131c0f7245c8f6ed1b9cda3138
mammukb <muhammedkb156@gmail.com>
2023-09-22 22:32:08 +0530

    LOgin Page of staff

---
+++ src/app/Staff/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../Components/Login'
+
+function page() {
+  return (
+    <div className='grid h-screen place-items-center  '  >
+        <Login/>
+    </div>
+  )
+}
+
+export default page
