3d10f963f10066a852f3ee41ebc5b9e995db8711
Ananthu TD <ananthutd2021@gmail.com>
2023-09-28 16:32:15 +0530

    added : menu

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Menu from './menu';
 
 function Admin() {
-    return <div>Admin pages</div>;
+    return <>
+        <Menu />
+    </>;
 }
 
 export default Admin;
