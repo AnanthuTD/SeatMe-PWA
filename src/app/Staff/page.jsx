462d181a7940c5957b64a9df76b38264442e5ed4
mammukb <muhammedkb156@gmail.com>
2023-09-29 22:06:35 +0530

    package.json

--- src/app/Staff/page.jsx
+++ src/app/Staff/page.jsx
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
-import React from 'react'
-import Login from '../components/Login'
 
-function page() {
-  return (
-    <div className='grid h-screen place-items-center sm:align-middle  '  >
-        <Login/>
-    </div>
-  )
-=======
+
+
 import React from 'react';
 import Login from '../login/page';
 
 function page() {
     return (<>staff</>
     );
->>>>>>> 613fe9cff74275272cb996ee8d6c2a86bfae0eea
+
 }
 
 export default page;
