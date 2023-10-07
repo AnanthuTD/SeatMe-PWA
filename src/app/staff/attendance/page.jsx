63345ed208f06a1011a7380a080307c8dc97d7ae
mammukb <muhammedkb156@gmail.com>
2023-10-07 22:47:34 +0530

    update in attendace page

--- src/app/staff/attendance/page.jsx
+++ src/app/staff/attendance/page.jsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Navbar from "../navbar";
-import { Button } from "antd";
+
+import Stdlist from "./stdlist";
 
 function page() {
 	let std = [
@@ -89,52 +90,31 @@ function page() {
 	];
 	const [isAbsent, setIsAbsent] = useState(false);
 	const [data, setData] = useState(std);
+    const [conform, setConform] = useState(false);
+	
+    const confirmpage = () => {
+		setConform[true];
+		console.log('hioeiufhedf');
+	}
 
-	const absent = (index) => {
-		data[index].isPresent = !data[index].isPresent;
-		setData([...data]);
-	};
+
 
 	return (
 		<>
 			<Navbar />
-			<div className="flex lg:flex-row gap-4  sm:flex-col lg:mt-14 flex-wrap sm:mt-8">
-				{data.map((student, index) => (
-					<div
-						className={` m-4 p-2 rounded-lg ${
-							student.isPresent ? "bg-green-500" : "bg-red-500"
-						}`}
-					>
-						<div className="flex flex-row items-center  justify-between">
-							<div className="text-blue-800">
-								<p>ID : {student.id} </p>
-								<p>Name : {student.name}</p>
-								<p>Program : {student.program}</p>
-								<span>
-									{" "}
-									Row : {student.row} col:{student.column}{" "}
-								</span>
-							</div>
-							{ student.isPresent ?
-                            
-                            <Button
-								danger
-								className="bg-red-400 text-white "
-								onClick={() => absent(index)}
-							>
-								Absent
-							</Button>  :   
-                            <Button
-                            danger
-                            className=""
-                            onClick={() => absent(index)}
-                        >
-                            present
-                        </Button>   }
-						</div>
-					</div>
-				))}
-			</div>
+			{ conform ? 
+			<div>hiii</div>
+			:
+			<>
+			
+			<Stdlist   data={data} setData={setData}      /> 
+			<button   onclick={ confirmpage }  className="p-5 bg-yellow-500"     >Submit</button> 
+			</>
+
+		
+		}
+			
+			
 		</>
 	);
 }
