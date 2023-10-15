3dc76f3a5fad07f6b3993e374400784f52d85a2d
Ananthu TD <ananthutd2021@gmail.com>
2023-10-15 23:27:53 +0530

    updated

--- src/app/admin/exam/assign/roomAssignmentForm.jsx
+++ src/app/admin/exam/assign/roomAssignmentForm.jsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from "react";
 import {
 	Form,
-	Input,
+	DatePicker,
 	Select,
 	Button,
 	Row,
@@ -11,8 +11,11 @@ import {
 	message,
 	Statistic,
 	Card,
+	Space,
+	Alert,
 } from "antd";
 import axios from "@/axiosInstance";
+import dayjs from "dayjs";
 
 const { Option } = Select;
 
@@ -22,6 +25,7 @@ const RoomAssignmentForm = ({ setIsSubmit }) => {
 	const [totalSeats, setTotalSeats] = useState(0);
 	const [examinesCount, setExaminesCount] = useState(0);
 	const [rooms, setRooms] = useState([]);
+	const [warningMessage, setWarningMessage] = useState("");
 	const [date, setDate] = useState(new Date());
 
 	const loadRooms = async () => {
@@ -29,18 +33,21 @@ const RoomAssignmentForm = ({ setIsSubmit }) => {
 		setRooms(result.data);
 	};
 
+	useEffect(() => {
+		loadRooms();
+	}, []);
+
 	const getExaminesCount = async () => {
 		const result = await axios.get("/api/admin/examines-count", {
 			params: {
 				date,
 			},
 		});
+		if (result.data === 0)
+			message.warning("No exams scheduled at this date");
+		message.info(result.data);
 		setExaminesCount(result.data);
 	};
-
-	useEffect(() => {
-		loadRooms();
-	}, []);
 
 	useEffect(() => {
 		getExaminesCount();
@@ -63,23 +70,23 @@ const RoomAssignmentForm = ({ setIsSubmit }) => {
 		setTotalSeats(newTotalSeats);
 	};
 
-	const filteredOptions = rooms.filter(
-		(o) => !selectedRoomIds.includes(o.id),
-	);
-
 	useEffect(() => {
 		calculateTotalSeats();
 	}, [selectedRoomIds]);
 
-	const onFinish = async () => {
+	const filteredOptions = rooms.filter(
+		(o) => !selectedRoomIds.includes(o.id),
+	);
+
+	const onFinish = async (values) => {
 		try {
 			const response = await axios.patch("/api/admin/rooms", {
 				roomIds: selectedRoomIds,
 			});
 
 			if (response.status === 200) {
-				message.success("PATCH request was successful");
-				setIsSubmit(true);
+				message.success("Room availability updated successfully");
+				assign(values);
 			} else {
 				message.error(
 					"PATCH request failed with status:",
@@ -92,6 +99,33 @@ const RoomAssignmentForm = ({ setIsSubmit }) => {
 		}
 	};
 
+	const assign = async (values) => {
+		try {
+			const { orderBy, selectedDate } = values;
+			const result = await axios.get("/api/admin/exam/assign", {
+				params: {
+					orderBy,
+					date: selectedDate,
+				},
+			});
+			if (result.status === 201)
+				message.success("Assignments successfully");
+			else if (result.status === 200) {
+				setWarningMessage(result.data.message);
+			}
+		} catch (error) {
+			if (
+				error.response &&
+				error.response.data &&
+				error.response.data.error
+			) {
+				message.error(error.response.data.error);
+			} else {
+				message.error("An error occurred while making the request.");
+			}
+		}
+	};
+
 	return (
 		<div style={{ padding: "20px" }}>
 			<Form
@@ -100,6 +134,29 @@ const RoomAssignmentForm = ({ setIsSubmit }) => {
 				layout="vertical"
 				onFinish={onFinish}
 			>
+				<Row gutter={16}>
+					<Col sm={24}>
+						<Form.Item
+							name="selectedDate"
+							initialValue={dayjs(date)}
+							rules={[
+								{
+									required: true,
+									message: "Please select a date",
+								},
+							]}
+						>
+							<DatePicker
+								style={{ width: 200 }}
+								format="YYYY-MM-DD"
+								className="date-picker"
+								onChange={(newDate) => {
+									setDate(newDate.toDate()); // Update the date when it changes
+								}}
+							/>
+						</Form.Item>
+					</Col>
+				</Row>
 				<Row gutter={16}>
 					<Col lg={19} md={24}>
 						<Form.Item
@@ -110,19 +167,18 @@ const RoomAssignmentForm = ({ setIsSubmit }) => {
 							<Select
 								mode="multiple"
 								showSearch
-								optionFilterProp="children"
 								filterOption={(input, option) =>
 									(option?.label ?? "").includes(input)
 								}
-								/* filterSort={(optionA, optionB) =>
-									(optionA?.children ?? "")
+								filterSort={(optionA, optionB) =>
+									(optionA?.label ?? "")
 										.toLowerCase()
 										.localeCompare(
 											(
-												optionB?.children ?? ""
+												optionB?.label ?? ""
 											).toLowerCase(),
 										)
-								} */
+								}
 								placeholder="Select rooms"
 								size="large"
 								onChange={setSelectedRoomIds}
@@ -134,7 +190,11 @@ const RoomAssignmentForm = ({ setIsSubmit }) => {
 								listHeight={350}
 							>
 								{filteredOptions.map((room) => (
-									<Option key={room.id} value={room.id} label={`r${room.id} f${room.floor} b${room.blockId}`}>
+									<Option
+										key={room.id}
+										value={room.id}
+										label={`r${room.id} f${room.floor} b${room.blockId}`}
+									>
 										<div
 											className="flex items-center justify-between"
 											style={{ padding: "8px" }}
@@ -183,17 +243,48 @@ const RoomAssignmentForm = ({ setIsSubmit }) => {
 						</Form.Item>
 					</Col>
 				</Row>
-
-				<Form.Item>
-					<Button
-						type="primary"
-						htmlType="submit"
-						disabled={totalSeats - examinesCount < 0}
-					>
-						Assign Rooms
-					</Button>
-				</Form.Item>
+				<Row gutter={16}>
+					<Col sm={24}>
+						<Form.Item
+							name="orderBy"
+							initialValue="rollNumber"
+							label="Sort by"
+							required={true}
+						>
+							<Select
+								style={{ width: 200 }}
+								className="select-box"
+							>
+								<Option value="rollNumber">Roll Number</Option>
+								<Option value="id">Registration Number</Option>
+							</Select>
+						</Form.Item>
+					</Col>
+					<Col sm={24}>
+						<Form.Item>
+							<Space>
+								<Button
+									type="primary"
+									className="assign-button"
+									htmlType="submit"
+								>
+									Assign
+								</Button>
+							</Space>
+						</Form.Item>
+					</Col>
+				</Row>
 			</Form>
+			{warningMessage?
+			<Row>
+				<Alert
+					message={"Add more rooms"}
+					description={warningMessage}
+					type="warning"
+					showIcon
+					/>
+			</Row>
+				:null}
 		</div>
 	);
 };
