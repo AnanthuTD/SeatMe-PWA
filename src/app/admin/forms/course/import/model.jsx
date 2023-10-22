987dfb9f8079d901126c81a667af22773ca50210
Ananthu TD <ananthutd2021@gmail.com>
2023-10-23 01:30:09 +0530

    added import from xlsx

---
+++ src/app/admin/forms/course/import/model.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { Button, Modal, Row, Col, Card, message } from "antd";
+import axios from "@/axiosInstance";
+
+const CoursesModel = ({ data, setData }) => {
+	const [loading, setLoading] = useState(false);
+	const [singleLoadings, setSingleLoadings] = useState(
+		new Array(data.length).fill(false),
+	);
+	const [open, setOpen] = useState(true);
+
+	const showModal = () => {
+		setOpen(true);
+	};
+	const handleOk = async () => {
+		setLoading(true);
+
+		try {
+			const result = await axios.patch("/api/admin/courses", data);
+			if (result.data.length) {
+				message.warning("Unable to update some records");
+				setData(result.data);
+			} else {
+				message.success("Updated successfully");
+				setData([]);
+			}
+		} catch (error) {
+			message.error("Update failed");
+		}
+		setLoading(false);
+	};
+	const handleCancel = () => {
+		setOpen(false);
+	};
+
+	const updateSingle = async (record, index) => {
+		const updatedLoadings = [...singleLoadings];
+		updatedLoadings[index] = true;
+
+		setSingleLoadings(updatedLoadings);
+
+		try {
+			const result = await axios.patch("/api/admin/courses", [record]);
+			if (result.data) {
+				message.success("Single record updated successfully");
+				const newData = [...data];
+				newData.splice(index, 1);
+				setData(newData);
+			} else message.error("Single record update failed");
+		} catch (error) {
+			message.error("Unknown error occurred");
+		}
+
+		updatedLoadings[index] = false;
+		setSingleLoadings(updatedLoadings);
+	};
+
+	return (
+		<>
+			<Modal
+				open={open}
+				title="Courses Model"
+				onOk={handleOk}
+				onCancel={handleCancel}
+				footer={[
+					<Button key="skip" onClick={handleCancel}>
+						Skip
+					</Button>,
+					<Button
+						key="update"
+						type="primary"
+						loading={loading}
+						onClick={handleOk}
+					>
+						Update
+					</Button>,
+				]}
+				closable={true}
+			>
+				{data.map((record, index) => (
+					<Card key={index} className="my-4">
+						<Row gutter={[16, 16]}>
+							<Col span={12}>
+								<p className="font-bold mb-1">Course Info:</p>
+
+								<p>Course ID: {record.id}</p>
+								<p>Course Name: {record.name}</p>
+								<p>Semester: {record.semester}</p>
+								<p>
+									Is Open Course:{" "}
+									{record.isOpenCourse ? "Yes" : "No"}
+								</p>
+							</Col>
+							{/* Add more fields as needed */}
+						</Row>
+						<Button
+							type="primary"
+							ghost
+							loading={singleLoadings[index]}
+							onClick={() => updateSingle(record, index)}
+						>
+							Update
+						</Button>
+					</Card>
+				))}
+			</Modal>
+		</>
+	);
+};
+
+export default CoursesModel;
---
+++ src/app/admin/forms/program/import/model.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import { Button, Modal, Row, Col, Card, message } from "antd";
+import axios from "@/axiosInstance";
+
+const ProgramModel = ({ data, setData }) => {
+	const [loading, setLoading] = useState(false);
+	const [singleLoadings, setSingleLoadings] = useState(
+		new Array(data.length).fill(false),
+	);
+	const [open, setOpen] = useState(true);
+
+	const showModal = () => {
+		setOpen(true);
+	};
+	const handleOk = async () => {
+		setLoading(true);
+
+		try {
+			const result = await axios.patch("/api/admin/program", data);
+			if (result.data.length) {
+				message.warning("Unable to update some records");
+				setData(result.data);
+			} else {
+				message.success("Updated successfully");
+				setData([]);
+			}
+		} catch (error) {
+			message.error("Update failed");
+		}
+		setLoading(false);
+	};
+	const handleCancel = () => {
+		setOpen(false);
+	};
+
+	const updateSingle = async (record, index) => {
+		const updatedLoadings = [...singleLoadings];
+		updatedLoadings[index] = true;
+
+		setSingleLoadings(updatedLoadings);
+
+		try {
+			const result = await axios.patch("/api/admin/program", [record]);
+			if (result.data) {
+				message.success("Single record updated successfully");
+				const newData = [...data];
+				newData.splice(index, 1);
+				setData(newData);
+			} else message.error("Single record update failed");
+		} catch (error) {
+			message.error("Unknown error occurred");
+		}
+
+		updatedLoadings[index] = false;
+		setSingleLoadings(updatedLoadings);
+	};
+
+	return (
+		<>
+			<Modal
+				open={open}
+				title="Program Model"
+				onOk={handleOk}
+				onCancel={handleCancel}
+				footer={[
+					<Button key="skip" onClick={handleCancel}>
+						Skip
+					</Button>,
+					<Button
+						key="update"
+						type="primary"
+						loading={loading}
+						onClick={handleOk}
+					>
+						Update
+					</Button>,
+				]}
+				closable={true}
+			>
+				{data.map((record, index) => (
+					<Card key={index} className="my-4">
+						<Row gutter={[16, 16]}>
+							<Col span={12}>
+								<p className="font-bold mb-1">Program Info:</p>
+
+								<p>Program ID: {record.id}</p>
+								<p>Program Name: {record.name}</p>
+								<p>
+									Aided or Unaided:{" "}
+									{record.aided ? "Aided" : "Unaided"}
+								</p>
+								<p>Department: {record.department}</p>
+								<p>Duration (Years): {record.duration}</p>
+								<p>Level: {record.level}</p>
+								{/* Add more fields as needed */}
+							</Col>
+							{/* You can add more columns as required */}
+						</Row>
+						<Button
+							type="primary"
+							ghost
+							loading={singleLoadings[index]}
+							onClick={() => updateSingle(record, index)}
+						>
+							Update
+						</Button>
+					</Card>
+				))}
+			</Modal>
+		</>
+	);
+};
+
+export default ProgramModel;
