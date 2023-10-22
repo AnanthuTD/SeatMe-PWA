985e32d282cf57bbed2e95bd268628200eaeb639
Ananthu TD <ananthutd2021@gmail.com>
2023-10-23 01:10:48 +0530

    added form

---
+++ src/app/admin/forms/course/page.jsx
@@ -0,0 +1,236 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import {
+	Input,
+	Button,
+	Row,
+	Col,
+	Form,
+	Divider,
+	Card,
+	message,
+	Alert,
+	Select,
+} from "antd";
+import { CloseOutlined } from "@ant-design/icons";
+import axios from "@/axiosInstance";
+import SelectProgram from "../../components/select";
+
+const DynamicCourseForm = () => {
+	const [form] = Form.useForm();
+	const [error, setError] = useState(null); // State to store error messages
+	const [programs, setPrograms] = useState([]); // State to store program data
+
+	const loadPrograms = async () => {
+		try {
+			const result = await axios.get("/api/admin/programs");
+			setPrograms(result.data);
+		} catch (error) {
+			console.error("Error fetching programs: ", error);
+		}
+	};
+
+	useEffect(() => {
+		loadPrograms();
+	}, []);
+
+	const handleSubmission = async (values) => {
+		console.log("Submitted values:", values);
+
+		try {
+			const result = await axios.post("/api/admin/courses", {
+				courses: values.courses,
+			});
+			if (result.status === 200) {
+				message.success(result.message);
+				setError(null); // Clear any previous errors
+			} else message.error("Submit failed");
+		} catch (error) {
+			console.log(error);
+			if (error.response.status === 400) {
+				message.error(
+					`Course with ID '${error.response.data.value}' already exists`,
+				);
+			} else {
+				setError("Something went wrong. Please try again."); // Set the error message
+			}
+		}
+	};
+
+	const onFinishFailed = (errorInfo) => {
+		message.warning(
+			"ID, Name, Semester, IsOpenCourse, and Program fields are required",
+		);
+	};
+
+	const handleAlertClose = () => {
+		setError(null); // Clear the error message
+	};
+
+	useEffect(() => {
+		form.setFieldsValue({ courses: [{}] });
+	}, [form]);
+
+	return (
+		<div className="p-3">
+			{error && (
+				<Alert
+					message="Error"
+					description={error}
+					type="error"
+					closable
+					onClose={handleAlertClose}
+					style={{ marginBottom: 16 }}
+				/>
+			)}
+			<Form
+				name="main"
+				onFinish={handleSubmission}
+				form={form}
+				initialValues={{
+					courses: [{}],
+				}}
+				onFinishFailed={onFinishFailed}
+			>
+				<Form.List name="courses">
+					{(fields, { add, remove }) => (
+						<div
+							style={{
+								display: "flex",
+								rowGap: 16,
+								flexDirection: "column",
+							}}
+						>
+							{fields.map((field) => (
+								<Card
+									size="small"
+									title={`Course ${field.name + 1}`}
+									key={field.key}
+									extra={
+										<CloseOutlined
+											onClick={() => {
+												remove(field.name);
+											}}
+										/>
+									}
+								>
+									<Row gutter={16}>
+										<Col xs={24} md={24} lg={7} xxl={7}>
+											<Form.Item
+												name={[field.name, "id"]}
+												label="Course ID"
+												rules={[
+													{
+														required: true,
+														message:
+															"Please enter the course ID",
+													},
+												]}
+											>
+												<Input />
+											</Form.Item>
+										</Col>
+										<Col xs={24} md={24} lg={10} xxl={10}>
+											<Form.Item
+												name={[field.name, "name"]}
+												label="Course Name"
+												rules={[
+													{
+														required: true,
+														message:
+															"Please enter the course name",
+													},
+												]}
+											>
+												<Input />
+											</Form.Item>
+										</Col>
+									</Row>
+									<Row gutter={16}>
+										<Col xs={24} md={24} lg={7} xxl={7}>
+											<Form.Item
+												name={[field.name, "semester"]}
+												label="Semester"
+												rules={[
+													{
+														required: true,
+														message:
+															"Please enter the semester",
+													},
+												]}
+											>
+												<Input />
+											</Form.Item>
+										</Col>
+										<Col xs={24} md={24} lg={7} xxl={7}>
+											<Form.Item
+												name={[
+													field.name,
+													"isOpenCourse",
+												]}
+												label="Is Open Course"
+												rules={[
+													{
+														required: true,
+														message:
+															"Please specify if it's an open course (true or false)",
+													},
+												]}
+											>
+												<Select
+													options={[
+														{
+															key: true,
+															value: true,
+														},
+														{
+															key: false,
+															value: false,
+														},
+													]}
+													placeholder="Select if open course or not"
+												/>
+											</Form.Item>
+										</Col>
+										<Col xs={24} md={24} lg={10} xxl={10}>
+											<Form.Item
+												name={[field.name, "program"]}
+												label="Program"
+												rules={[
+													{
+														required: true,
+														message:
+															"Please select the program",
+													},
+												]}
+											>
+												<SelectProgram
+													options={programs}
+													placeholder="Select Program"
+												/>
+											</Form.Item>
+										</Col>
+									</Row>
+								</Card>
+							))}
+							<Button type="dashed" onClick={() => add()} block>
+								+ Add Course
+							</Button>
+						</div>
+					)}
+				</Form.List>
+				<Divider />
+				<Row gutter={16}>
+					<Col sm={24} md={5}>
+						<Button ghost type="primary" htmlType="submit">
+							Submit All
+						</Button>
+					</Col>
+				</Row>
+			</Form>
+		</div>
+	);
+};
+
+export default DynamicCourseForm;
---
+++ src/app/admin/forms/program/page.jsx
@@ -0,0 +1,237 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import {
+	Input,
+	Button,
+	Row,
+	Col,
+	Form,
+	Divider,
+	Card,
+	message,
+	Alert,
+	Select,
+} from "antd";
+import { CloseOutlined } from "@ant-design/icons";
+import axios from "@/axiosInstance";
+import SelectDepartment from "../../components/select";
+
+const DynamicProgramForm = () => {
+	const [form] = Form.useForm();
+	const [error, setError] = useState(null); // State to store error messages
+	const [departments, setDepartments] = useState([]); // State to store department data
+
+	const loadDepartments = async () => {
+		try {
+			const result = await axios.get("/api/admin/departments");
+			setDepartments(result.data);
+		} catch (error) {
+			console.error("Error fetching departments: ", error);
+		}
+	};
+
+	useEffect(() => {
+		loadDepartments();
+	}, []);
+
+	const handleSubmission = async (values) => {
+		console.log("Submitted values:", values);
+
+		try {
+			const result = await axios.post("/api/admin/programs", {
+				programs: values.programs,
+			});
+			if (result.status === 200) {
+				message.success(result.message);
+				setError(null); // Clear any previous errors
+			} else message.error("Submit failed");
+		} catch (error) {
+			console.log(error);
+			if (error.response.status === 400) {
+				message.error(
+					`Program with ID '${error.response.data.value}' already exists`,
+				);
+			} else {
+				setError("Something went wrong. Please try again."); // Set the error message
+			}
+		}
+	};
+
+	const onFinishFailed = (errorInfo) => {
+		message.warning(
+			"ID, Name, Department, Duration, and Level are required",
+		);
+	};
+
+	const handleAlertClose = () => {
+		setError(null); // Clear the error message
+	};
+
+	useEffect(() => {
+		form.setFieldsValue({ programs: [{}] });
+	}, [form]);
+
+	return (
+		<div className="p-3">
+			{error && (
+				<Alert
+					message="Error"
+					description={error}
+					type="error"
+					closable
+					onClose={handleAlertClose}
+					style={{ marginBottom: 16 }}
+				/>
+			)}
+			<Form
+				name="main"
+				onFinish={handleSubmission}
+				form={form}
+				initialValues={{
+					programs: [{}],
+				}}
+				onFinishFailed={onFinishFailed}
+			>
+				<Form.List name="programs">
+					{(fields, { add, remove }) => (
+						<div
+							style={{
+								display: "flex",
+								rowGap: 16,
+								flexDirection: "column",
+							}}
+						>
+							{fields.map((field) => (
+								<Card
+									size="small"
+									title={`Program ${field.name + 1}`}
+									key={field.key}
+									extra={
+										<CloseOutlined
+											onClick={() => {
+												remove(field.name);
+											}}
+										/>
+									}
+								>
+									<Row gutter={16}>
+										<Col xs={24} md={24} lg={7} xxl={7}>
+											<Form.Item
+												name={[field.name, "id"]}
+												label="Program ID"
+												rules={[
+													{
+														required: true,
+														message:
+															"Please enter the program ID",
+													},
+												]}
+											>
+												<Input />
+											</Form.Item>
+										</Col>
+										<Col xs={24} md={24} lg={10} xxl={10}>
+											<Form.Item
+												name={[field.name, "name"]}
+												label="Program Name"
+												rules={[
+													{
+														required: true,
+														message:
+															"Please enter the program name",
+													},
+												]}
+											>
+												<Input />
+											</Form.Item>
+										</Col>
+									</Row>
+									<Row gutter={16}>
+										<Col xs={24} md={24} lg={7} xxl={7}>
+											<Form.Item
+												name={[
+													field.name,
+													"department",
+												]}
+												label="Department"
+												rules={[
+													{
+														required: true,
+														message:
+															"Please select the department",
+													},
+												]}
+											>
+												<SelectDepartment
+													options={departments}
+													placeholder="Select Department"
+												/>
+											</Form.Item>
+										</Col>
+										<Col xs={24} md={24} lg={7} xxl={7}>
+											<Form.Item
+												name={[field.name, "duration"]}
+												label="Duration (Years)"
+												rules={[
+													{
+														required: true,
+														message:
+															"Please enter the duration in years",
+													},
+												]}
+											>
+												<Input />
+											</Form.Item>
+										</Col>
+										<Col xs={24} md={24} lg={7} xxl={7}>
+											<Form.Item
+												name={[field.name, "level"]}
+												label="Level"
+												rules={[
+													{
+														required: true,
+														message:
+															"Please enter the level (UG or PG)",
+													},
+												]}
+											>
+												<Select
+													defaultValue={"UG"}
+													options={[
+														{
+															key: "ug",
+															value: "UG",
+														},
+														{
+															key: "pg",
+															value: "PG",
+														},
+													]}
+													placeholder="Select Department"
+												/>
+											</Form.Item>
+										</Col>
+									</Row>
+								</Card>
+							))}
+							<Button type="dashed" onClick={() => add()} block>
+								+ Add Program
+							</Button>
+						</div>
+					)}
+				</Form.List>
+				<Divider />
+				<Row gutter={16}>
+					<Col sm={24} md={5}>
+						<Button ghost type="primary" htmlType="submit">
+							Submit All
+						</Button>
+					</Col>
+				</Row>
+			</Form>
+		</div>
+	);
+};
+
+export default DynamicProgramForm;
