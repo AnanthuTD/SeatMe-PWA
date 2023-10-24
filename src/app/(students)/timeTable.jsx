d577056fc907771f965919304c18a4a9fca3d88e
Ananthu TD <ananthutd2021@gmail.com>
2023-10-24 16:02:41 +0530

    updated

--- src/app/(students)/timeTable.jsx
+++ src/app/(students)/timeTable.jsx
@@ -1,58 +1,42 @@
 import React from "react";
-import { List } from "antd";
-
-/* const data = [
-	["25/6/2023", "METHODOLOGY OF PROGRAMMING", "1:30-4:30"],
-	["25/6/2023", "METHODOLOGY OF PROGRAMMING", "1:30-4:30"],
-	["25/6/2023", "METHODOLOGY OF PROGRAMMING", "1:30-4:30"],
-	["25/6/2023", "METHODOLOGY OF PROGRAMMING", "1:30-4:30"],
-	["25/6/2023", "METHODOLOGY OF PROGRAMMING", "1:30-4:30"],
-]; */
+import { Descriptions } from "antd";
 
 const TimeTable = ({ data }) => {
-	const convertedData = data
-		.map((value) => {
-			const dateParts = value.date?.split("-");
-			const formattedDate = dateParts
-				? `${dateParts[2]}/${dateParts[1]}/${dateParts[0]}`
-				: "not issued";
-			return [
-				formattedDate,
-				value.courseName,
-				`${value.timeCode ? value.timeCode : "not issued"}`,
-			];
-		})
-		
+	const convertedData = data.map((value) => {
+		const dateParts = value.date?.split("-");
+		const formattedDate = dateParts
+			? `${dateParts[2]}/${dateParts[1]}/${dateParts[0]}`
+			: "not issued";
+		return {
+			date: formattedDate,
+			courseName: value.courseName,
+			timeCode: value.timeCode ? value.timeCode : "not issued",
+		};
+	});
 
-	// Log the converted data
-	// console.log(convertedData);
 	return (
-		<>
-			<List
-				grid={{
-					gutter: 16,
-					xs: 1,
-					sm: 1,
-					md: 1,
-					lg: 1,
-					xl: 1,
-					xxl: 3,
-				}}
-				size="small"
-				dataSource={convertedData}
-				// style={{overflow:'hidden', overflowY:'scroll'}}
-				renderItem={(item) => (
-					<List.Item>
-						<List
-							size="small"
-							bordered
-							dataSource={item}
-							renderItem={(item) => <List.Item>{item}</List.Item>}
-						/>
-					</List.Item>
-				)}
-			/>
-		</>
+		<div>
+			{convertedData.map((item, index) => (
+				<div key={index} className="mb-4">
+					{" "}
+					<Descriptions
+						bordered
+						column={{ xs: 1, sm: 1, md: 1, lg: 1, xl: 1, xxl: 3 }}
+					>
+						<Descriptions.Item label="Date">
+							{item.date}
+						</Descriptions.Item>
+						<Descriptions.Item label="Course Name">
+							{item.courseName}
+						</Descriptions.Item>
+						<Descriptions.Item label="Time">
+							{item.timeCode}
+						</Descriptions.Item>
+					</Descriptions>
+				</div>
+			))}
+		</div>
 	);
 };
+
 export default TimeTable;
