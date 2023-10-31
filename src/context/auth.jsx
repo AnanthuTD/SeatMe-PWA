fae667f2bc7f6b5596e9d5daffda896256e0df30
Ananthu TD <ananthutd2021@gmail.com>
2023-10-31 20:54:57 +0530

    check if authenticated . redirect to login if not.

---
+++ src/context/auth.jsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { permanentRedirect } from "next/navigation";
+import axios from "@/lib/axiosInstance";
+import { Spin, Alert, Button } from "antd";
+
+export const LoadingProvider = ({ children, api }) => {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		axios
+			.get(api)
+			.then((response) => {
+				setLoading(false);
+			})
+			.catch((error) => {
+				if (error.response && error.response.status === 401) {
+					permanentRedirect("/login", "replace");
+				} else {
+					setLoading(false); // Set loading to false for non-401 errors.
+					console.error("API call error:", error);
+					setError(error.response.data); // Set the error message.
+				}
+			});
+	}, []);
+
+	const [error, setError] = useState(null); // State to hold the error message.
+
+	return (
+		<>
+			{loading ? (
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						height: "100vh",
+					}}
+				>
+					<Spin size="large" />
+				</div>
+			) : error ? (
+				<div className="mx-auto mt-8 aspect-square flex justify-center items-center h-full">
+					<div
+						className="bg-white p-4 border border-gray-300 rounded shadow w-1/2"
+						style={{ width: "50%"}}
+					>
+						<Alert
+							message="Error"
+							description={error}
+							type="error"
+							showIcon
+							onClose={() => setError(null)}
+						/>
+						<div className="text-center mt-4 ">
+							<Button type="primary" href="/login">
+								Login
+							</Button>
+						</div>
+					</div>
+				</div>
+			) : (
+				children
+			)}
+		</>
+	);
+};
