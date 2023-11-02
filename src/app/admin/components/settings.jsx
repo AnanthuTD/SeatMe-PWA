a7022838ad4f284a8bce9d18aead093594b5ac92
Ananthu TD <ananthutd2021@gmail.com>
2023-11-02 16:12:03 +0530

    feat: userInfo

--- src/app/admin/components/settings.jsx
+++ src/app/admin/components/settings.jsx
@@ -9,6 +9,8 @@ import {
 	Row,
 	Col,
 	message,
+	Descriptions,
+	Menu,
 } from "antd";
 import {
 	UserOutlined,
@@ -19,10 +21,9 @@ import {
 import axios from "axios";
 import { setAuthorizationToken } from "@/lib/axiosPrivate";
 import { useRouter } from "next/navigation";
+import { useAccount } from "@/context/accountContext";
 
-const { Link } = Anchor;
-
-const SettingsButton = ({ profilePicture }) => {
+const SettingsButton = () => {
 	const [visible, setVisible] = useState(false);
 
 	const router = useRouter();
@@ -52,9 +53,65 @@ const SettingsButton = ({ profilePicture }) => {
 		}
 	};
 
+	const onFinish = (values) => {};
+
+	const items = [
+		{
+			key: "viewProfile",
+			href: "#viewProfile",
+			title: "Profile",
+			// label: "Profile",
+		},
+		{
+			key: "viewNotifications",
+			href: "#viewNotifications",
+			title: "Notifications",
+			// label: "Notifications",
+		},
+		{
+			key: "editProfile",
+			href: "#editProfile",
+			title: "Edit Profile",
+			// label: "Edit Profile",
+		},
+	];
+
+	const { user } = useAccount();
+
+	console.log(user);
+
+	const descriptionItems = [
+		{
+			key: "6",
+			label: "Id",
+			children: user?.id,
+		},
+		{
+			key: "1",
+			label: "UserName",
+			children: user?.name,
+		},
+		{
+			key: "5",
+			label: "Designation",
+			children: user?.designation || "Empty",
+		},
+		{
+			key: "2",
+			label: "Telephone",
+			children: user?.phone || "Empty",
+		},
+		{
+			key: "4",
+			label: "Address",
+			span: 2,
+			children: user?.address,
+		},
+	];
+
 	return (
 		<>
-			{profilePicture ? (
+			{user?.profilePicture ? (
 				<Button
 					type="primary"
 					shape="circle"
@@ -79,19 +136,62 @@ const SettingsButton = ({ profilePicture }) => {
 				open={visible}
 				onCancel={handleCancel}
 				footer={null}
+				centered
+				
 			>
-				<Row gutter={16}>
-					<Col span={8}>
-						<Anchor affix={false} style={{ height: "100%" }}>
-							<Link href="#viewProfile" title="View Profile" />
-							<Link href="#editProfile" title="Edit Profile" />
-							<Link
-								href="#profileSettings"
-								title="Profile Settings"
-							/>
-						</Anchor>
+				<Row
+					gutter={16}
+					style={{ maxHeight: "400px", overflowY: "auto" }}
+				>
+					<Col span={6}>
+						<Anchor
+							affix={false}
+							items={items}
+							
+							replace={true}
+						/>
+						{/* <Menu items={items} /> */}
 					</Col>
-					<Col span={16}></Col>
+					<Col
+						span={18}
+						style={{ maxHeight: "400px", overflowY: "auto" }}
+						className="scrollbar-none"
+					>
+						<Descriptions
+							id="viewProfile"
+							title="User Profile"
+							// bordered
+							layout="vertical"
+							items={descriptionItems}
+							column={2}
+						/>
+
+						<Form
+							id="editProfile"
+							layout="vertical"
+							onFinish={onFinish}
+						>
+							<Form.Item
+								label="Name"
+								name="name"
+								initialValue="John Doe"
+							>
+								<Input />
+							</Form.Item>
+							<Form.Item
+								label="Email"
+								name="email"
+								initialValue="johndoe@example.com"
+							>
+								<Input />
+							</Form.Item>
+							<Form.Item>
+								<Button type="primary" htmlType="submit">
+									Save Profile
+								</Button>
+							</Form.Item>
+						</Form>
+					</Col>
 				</Row>
 
 				<div style={{ marginTop: 20 }}>
