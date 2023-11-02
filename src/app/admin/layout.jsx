d9b758ce26fe534b1d3722e48242d4a2954c7889
Ananthu TD <ananthutd2021@gmail.com>
2023-11-02 14:40:16 +0530

    commented unnecessary menuContext

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import theme from "./themeConfig";
 import { ConfigProvider } from "antd";
 import Layout from "./layoutProvider";
-import { MenuProvider } from "./(menu)/menuContext";
+// import { MenuProvider } from "./(menu)/menuContext";
 import { AuthProvider } from "@/context/auth";
 
 const RootLayout = ({ children }) => {
 	const authApi = "/api/admin";
 	return (
-		<ConfigProvider theme={theme}>
-			<AuthProvider api={authApi}>
-				<MenuProvider>
-					<Layout>{children}</Layout>
-				</MenuProvider>
-			</AuthProvider>
-		</ConfigProvider>
+		<AuthProvider api={authApi}>
+			<ConfigProvider theme={theme}>
+				{/* <MenuProvider> */}
+				<Layout>{children}</Layout>
+				{/* </MenuProvider> */}
+			</ConfigProvider>
+		</AuthProvider>
 	);
 };
 
