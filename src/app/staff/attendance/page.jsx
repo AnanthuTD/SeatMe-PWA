527f3134077e373e3574515d8f1310b8c0238253
mammukb <muhammedkb156@gmail.com>
2023-11-12 10:06:50 +0530

    staff page

--- src/app/staff/attendance/page.jsx
+++ src/app/staff/attendance/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 "use strict";
 
-
 import React, { useEffect, useState } from "react";
 import Navbar from "../navbar";
 import { Button } from "antd";
@@ -9,165 +8,67 @@ import Absentstds from "./absentstds";
 import Link from "next/link";
 import axios from "@/lib/axiosPrivate";
 
-
-
 import Stdlist from "./stdlist";
+const onDuty = localStorage.getItem('variableonDuty');
 
 function page() {
-/*	let std = [
-		{
-			id: 1,
-			name: "Muhammed",
-			program: "Bca",
-			row: 1,
-			column: 6,
-			isPresent: true,
-		},
-		{
-			id: 2,
-			name: "Muhammed",
-			program: "Bca",
-			row: 1,
-			column: 6,
-			isPresent: true,
-		},
-		{
-			id: 3,
-			name: "Muhammed",
-			program: "Bca",
-			row: 1,
-			column: 6,
-			isPresent: true,
-		},
-		{
-			id: 4,
-			name: "Muhammed",
-			program: "Bca",
-			row: 1,
-			column: 6,
-			isPresent: true,
-		},
-		{
-			id: 5,
-			name: "Muhammed",
-			program: "Bca",
-			row: 1,
-			column: 6,
-			isPresent: true,
-		},
-		{
-			id: 6,
-			name: "Muhammed",
-			program: "Bca",
-			row: 1,
-			column: 6,
-			isPresent: true,
-		},
-		{
-			id: 7,
-			name: "Muhammed",
-			program: "Bca",
-			row: 1,
-			column: 6,
-			isPresent: true,
-		},
-		{
-			id: 8,
-			name: "Muhammed",
-			program: "Bca",
-			row: 1,
-			column: 6,
-			isPresent: true,
-		},
-		{
-			id: 9,
-			name: "Muhammed",
-			program: "Bca",
-			row: 1,
-			column: 6,
-			isPresent: true,
-		},
-		{
-			id: 10,
-			name: "Muhammed",
-			program: "Bca",
-			row: 1,
-			column: 6,
-			isPresent: true,
-		},
-	];  */
 	const [isAbsent, setIsAbsent] = useState(false);
 	const [data, setData] = useState([]);
-    const  [conform, setConform] = useState(false);
+	const [conform, setConform] = useState(false);
+	console.log(onDuty);
 
-    const roomid = "4";
+	const roomid = "4";
 	useEffect(() => {
-		axios.get(`/api/staff/attendance/${roomid}`)
-		.then((response) => {
-		  let std = response.data;
-		  console.log(std);
-		  setData(std);
+		axios
+			.get(`/api/staff/attendance/${roomid}`)
+			.then((response) => {
+				let std = response.data;
+				console.log(std);
+				setData(std);
+			})
+			.catch((error) => {
+				if (error.response) {
+					console.error(
+						"Server responded with status code:",
+						error.response.status,
+					);
+					console.error("Server response data:", error.response.data);
+				} else {
+					console.error("Request failed:", error.message);
+				}
+			});
+	}, []);
 
-		})
-		.catch((error) => {
-		  if (error.response) {
-			console.error("Server responded with status code:", error.response.status);
-			console.error("Server response data:", error.response.data);
-		  } else {
-			console.error("Request failed:", error.message);
-		  }
-		});
-
-
-
-	},
-	
-	
-	[])
-	
-	
-	  
-
-
-
-
-	
-	
-    const confirmpage = () => {	
-		setConform(!conform);	
-	}
-   
-	
+	const confirmpage = () => {
+		setConform(!conform);
+	};
 
 	return (
 		<>
 			<Navbar />
-			{ conform ? 
-			<>
-			   <h1 className="text-center text-2xl text-gray-700  mt-5 "   >Check and click <span className="text-blue-500"  >finish</span></h1>
-              <Absentstds  data={data} conform={conform} setConform={setConform}  />
-			 
-
-
-
-			</>
-			:
-			<>
-			
-			<Stdlist   data={data} setData={setData}   
-			   /> 
-			<button
-								className=" ml-36 mt-5 mb-4  px-4 py-3 bg-blue-600  rounded-3xl border-4 "
-								onClick={() => confirmpage()}
-							>
-								<p  className="text-lg  text-white  "     >    Submit</p>
-							</button>
-			</>
-
-		
-		}
-			
-			
+			{conform ? (
+				<>
+					<h1 className="text-center text-2xl text-gray-700  mt-5 ">
+						Check and click{" "}
+						<span className="text-blue-500">finish</span>
+					</h1>
+					<Absentstds
+						data={data}
+						conform={conform}
+						setConform={setConform}
+					/>
+				</>
+			) : (
+				<>
+					<Stdlist data={data} setData={setData} />
+					<button
+						className=" ml-36 mt-5 mb-4  px-4 py-3 bg-blue-600  rounded-3xl border-4 "
+						onClick={() => confirmpage()}
+					>
+						<p className="text-lg  text-white  "> Submit</p>
+					</button>
+				</>
+			)}
 		</>
 	);
 }
--- src/app/staff/page.jsx
+++ src/app/staff/page.jsx
@@ -5,9 +5,35 @@ import Navbar from "./navbar";
 import Calendar from "./calender";
 import RoomDetails from "./roomDetails";
 import Offduty from "./offduty";
+import axios from "@/lib/axiosPrivate";
+import { useState , useEffect } from "react";
 
 function page() {
-	const onDuty = true;
+	
+	const [onDuty, setOnDuty] = useState(false);
+
+	useEffect(() => {
+		axios
+			.get("/api/staff")
+			.then((response) => {
+				let tchr = response.data;
+				console.log(tchr);
+				const onDutyValue = tchr.onDuty;
+				setOnDuty(onDutyValue);
+				localStorage.setItem('variableonDuty',onDuty);
+			})
+			.catch((error) => {
+				if (error.response) {
+					console.error(
+						"Server responded with status code:",
+						error.response.status,
+					);
+					console.error("Server response data:", error.response.data);
+				} else {
+					console.error("Request failed:", error.message);
+				}
+			});
+	}, []);
 
 	return (
 		<div>
