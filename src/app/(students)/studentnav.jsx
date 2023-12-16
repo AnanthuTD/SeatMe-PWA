422b5196f36476b2d1ee4c4a59349dae0bc0cc5f
mammukb <muhammedkb156@gmail.com>
2023-12-16 09:59:01 +0530

    colour cxhange

--- src/app/(students)/studentnav.jsx
+++ src/app/(students)/studentnav.jsx
@@ -8,7 +8,7 @@ import { MdOutlineEventSeat } from "react-icons/md";
 export default function studentnav() {
   return (
     <>
-	    <div className=' navhead_title  h-12 p-1  w-full  bg-blue-800  flex flex-row items-center justify-center gap-2 '     >
+	    <div className=' navhead_title  h-12 p-1  w-full  bg-blue-700  flex flex-row items-center justify-center gap-2 '     >
 		
 		 <MdOutlineEventSeat className='h-11 w-11' />
 			<h3 className='text-white  m-0 mt-2 text-3xl'  >Seatme</h3 >	  
