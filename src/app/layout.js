d691516f9a637067dd5f66760aad40f15331a6a6
Ananthu TD <ananthutd2021@gmail.com>
2024-01-13 22:10:28 +0530

    added google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 import StyledComponentsRegistry from "@/lib/AntdRegistry";
 
@@ -17,6 +18,19 @@ import { AccountProvider } from "@/context/accountContext";
 const RootLayout = ({ children }) => {
 	return (
 		<html lang="en">
+			<Script
+				async
+				src="https://www.googletagmanager.com/gtag/js?id=G-4Z973J09EJ"
+			></Script>
+			<Script>
+				{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-4Z973J09EJ');
+        `}
+			</Script>
 			{/* <link rel="manifest" href="/manifest.json" /> */}
 			<link rel="apple-touch-icon" href="/next.svg"></link>
 			<meta name="theme-color" content="#fff" />
